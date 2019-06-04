3278b9652a0781eb13e8ffebd5314a619917b63e
konsul7770 <33548328+konsul7770@users.noreply.github.com>
2019-06-04 15:24:12 +0300

    Add files via upload

---
+++ projectapi/src/server/db/connection.js
@@ -0,0 +1,4 @@
+﻿const environment = process.env.NODE_ENV || 'development';
+const config = require('../../../knexfile.js')[environment];
+
+module.exports = require('knex')(config);
---
+++ projectapi/src/server/routes/index.js
@@ -0,0 +1,11 @@
+﻿const Router = require('koa-router');
+const router = new Router();
+
+router.get('/', async (ctx) => {
+  ctx.body = {
+    status: 'success',
+    message: 'hello, world!'
+  };
+})
+
+module.exports = router;
